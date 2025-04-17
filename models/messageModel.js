6b83d1156ff583472a21a248dea929ac849d8390
Arun023 <arundev8153@gmail.com>
2025-04-18 02:30:33 +0530

    feat: chat, message, user model

---
+++ models/messageModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  //   receiver: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'User',
+  //     required: true,
+  //   },
+  chat: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Chat',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  timestamps: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+  },
+  profilePic: {
+    type: String,
+    default:
+      'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
